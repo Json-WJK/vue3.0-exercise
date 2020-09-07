0e371897ed2d167a3af4a5e0074b44896b07d16c
王俊坤 <920819994@qq.com>
2020-09-07 15:56:51 +0800

    子组件使用方法

--- src/components/basiceComponent.vue
+++ src/components/basiceComponent.vue
@@ -1,15 +1,19 @@
 <template>
   <div>我是basice子组件</div>
   <button @click="componentClick">点我给父组件传递参数</button>
+  <div>子组件接收的参数count: {{ count }}</div>
 </template>
 
 <script>
 export default {
   props: {
-    count: Number
+    count: {
+      default: 0,
+      type: Number
+    }
   },
   setup(props, { emit }) {
-    console.log(props, "传递的参数");
+    console.log(props.count, "传递的参数");
     const componentClick = () => {
       emit("componentClick", 1);
     };
