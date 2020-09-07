885ee4a675594a03022e4776f620d7bc1a2625e4
王俊坤 <920819994@qq.com>
2020-09-07 14:48:34 +0800

    路由调整

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'basics',
+    name: '',
     component: () => import(/* webpackChunkName: "about" */ '../views/basics.vue')
   },
   {
